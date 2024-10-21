7272c91042e5cbf8a3b8e95a256b35532a70bf16
Sreyas-A-S <sreyasas25@gmail.com>
2024-10-21 17:13:41 +0530

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/suppliers', function () {
+    return view('suppliers');
+});
+
+Route::get('/items', function () {
+    return view('items');
+});
+
+Route::get('/orders', function () {
+    return view('orders');
+});
