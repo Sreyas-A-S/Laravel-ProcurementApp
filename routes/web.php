30574bf52faeecdf1c046276bf176320ea50a350
Sreyas-A-S <sreyasas25@gmail.com>
2024-10-21 17:52:27 +0530

    SupplierController has beeen created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupplierController;
 
 Route::get('/', function () {
     return view('index');
@@ -17,3 +18,6 @@ Route::get('/items', function () {
 Route::get('/orders', function () {
     return view('orders');
 });
+
+
+Route::get('suppliers/data', [SupplierController::class, 'getData'])->name('suppliers.data');
