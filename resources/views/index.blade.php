5e7881054ea689968e47440d64bf1a318201f34c
Sreyas-A-S <sreyasas25@gmail.com>
2024-10-21 17:00:41 +0530

    Layout modifications

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,37 +1,84 @@
 
+@extends('layouts.app')
 
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Suppliers</h5>
-                        <p class="card-text">Manage your suppliers.</p>
-                        <a href="{{ 'hi' }}" class="btn btn-primary">View Suppliers</a>
-                    </div>
-                </div>
-            </div>
+@section('title', 'Home Page')
 
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Items</h5>
-                        <p class="card-text">Manage your items.</p>
-                        <a href="{{ "route('items.index')" }}" class="btn btn-primary">View Items</a>
-                    </div>
-                </div>
-            </div>
+@section('content')
 
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Purchase Orders</h5>
-                        <p class="card-text">Manage your purchase orders.</p>
-                        <a href="{{ 'hi2' }}" class="btn btn-primary">View Purchase Orders</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="overflow-hidden">
+		<div class="container">
+			<div class="max-w-2xl mx-auto text-center pt-3 pb-2">
+				<h2 class="m-0 text-primary-emphasis text-base leading-7 fw-semibold">
+					Find the right plan for your business
+				</h2>
+				
+			</div>
+		</div>
 
+		
+
+		<div class="container my-3">
+			<div class="row align-items-center align-items-xl-stretch flex-column flex-xl-row">
+				<div class="col-12 col-sm-10 col-md-7 col-lg-6 col-xl-4 mt-5">
+					<div class="p-5 h-100 d-flex flex-column bg-body-tertiary shadow rounded-4">
+						<h2>Suppliers</h2>
+						<div class="my-2">
+							<span class="fs-1 text-body-secondary fw-bold">$</span>
+							<span class="fs-1 text-dak fw-bolder essential">49</span>
+							<span class="fs-3 text-body-secondary">/mo</span>
+						</div>
+						
+						<hr class="opacity-10">
+						
+						<div class="mt-auto">
+							<button class="btn btn-primary text-white mt-4 w-100">
+								View
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-12 col-sm-10 col-md-7 col-lg-6 col-xl-4 mt-5">
+					<div class="p-5 h-100 d-flex flex-column bg-body-tertiary shadow rounded-4">
+						<h2>Items</h2>
+						<div class="my-2">
+							<span class="fs-1 text-body-secondary fw-bold">$</span>
+							<span class="fs-1 text-dak fw-bolder essential">49</span>
+							<span class="fs-3 text-body-secondary">/mo</span>
+						</div>
+						
+						<hr class="opacity-10">
+						
+						<div class="mt-auto">
+							<button class="btn btn-primary text-white mt-4 w-100">
+								View
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-12 col-sm-10 col-md-7 col-lg-6 col-xl-4 mt-5">
+					<div class="p-5 h-100 d-flex flex-column bg-body-tertiary shadow rounded-4">
+						<h2>Purchase Orders</h2>
+						<div class="my-2">
+							<span class="fs-1 text-body-secondary fw-bold">$</span>
+							<span class="fs-1 text-dak fw-bolder essential">49</span>
+							<span class="fs-3 text-body-secondary">/mo</span>
+						</div>
+						
+						<hr class="opacity-10">
+						
+						<div class="mt-auto">
+							<button class="btn btn-primary text-white mt-4 w-100">
+								View
+							</button>
+						</div>
+					</div>
+				</div>
+
+				
+			</div>
+		</div>
+	</div>
    
+    @endsection
